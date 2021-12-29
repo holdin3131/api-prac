eb0ce660b2a1afc58461fcba845e456b0747589a
holdin3131 <holdin3131@gmail.com>
2021-12-29 12:08:11 +0900

    add: get post

--- prac/app.py
+++ prac/app.py
@@ -1,11 +1,27 @@
-from flask import Flask ,render_template   # 랜더 템플릿 추가
+from flask import Flask, render_template, request,jsonify
+#add requset ,jsonify
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
-   return render_template('index.html')    # 랜더 템플릿(html 파일)
+    return render_template('index.html')  # 랜더 템플릿(html 파일)
 
 
+@app.route('/test', methods=['GET'])
+def test_get():
 
-if __name__ == '__main__':  
-   app.run('0.0.0.0',port=5000,debug=True)
+    title_receive = request.args.get('title_give')
+    print(title_receive)
+    return jsonify({'result': 'success', 'msg': '이 요청은 GET!'})
+
+@app.route('/test', methods=['POST'])
+def test_post():
+
+
+    title_receive = request.form['title_give']
+    print(title_receive)
+    return jsonify({'result': 'success', 'msg': '요청을 잘 받았습니다.'})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
